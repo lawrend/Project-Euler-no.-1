35e0f3f1d9040a6bcc8dd8ca927c42d8039e7b7c
lawrend <douglas_lawrence@mac.com>
2015-06-27 22:58:09 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 var numbersArray = [];
-var Threes = [];
-var Fives = [];
+var threes = [];
+var fives = [];
 var threeFives = [];
-var AllThreesAndFives = Threes.concat(Fives, threeFives);
+var AllThreesAndFives = threes.concat(fives, threeFives);
 var totalSum = 0;
 
 //create an array of the numbers 1-999.
 for(var x = 1; x < 1000; x++) numbersArray.push(x); 
 
 //pushed numbers divisible by 3, by 5, and by both into 3 diff arrays.  not optimal.
-for(var y = 0; y < numbersArray.length; y++) if (numbersArray[y] % 3 === 0 && numbersArray[y] % 5 !== 0) {Threes.push(numbersArray[y]);} 
+for(var y = 0; y < numbersArray.length; y++) if (numbersArray[y] % 3 === 0 && numbersArray[y] % 5 !== 0) {threes.push(numbersArray[y]);} 
 
-    else if (numbersArray[y] % 5 ===  0 && numbersArray[y] % 3 !== 0) {Fives.push(numbersArray[y]);}
+    else if (numbersArray[y] % 5 ===  0 && numbersArray[y] % 3 !== 0) {fives.push(numbersArray[y]);}
 
     else if (numbersArray[y] % 3 === 0 && numbersArray[y] % 5 === 0)     {threeFives.push(numbersArray[y]);}
 
